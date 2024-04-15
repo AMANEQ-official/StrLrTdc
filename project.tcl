3518ead6f98e63b8690af685d233c2b207bee916
Ryotaro Honda <honda@lambda.phys.tohoku.ac.jp>
2024-04-16 05:18:22 +0900

    Mod project.tcl

--- project.tcl
+++ project.tcl
@@ -286,7 +286,7 @@ proc checkRequiredFiles { origin_dir} {
  "[file normalize "$origin_dir/hdl/strtdc/lrtdc-impl/strLrTdc.vhd"]"\
  "[file normalize "$origin_dir/hdl/toplevel.vhd"]"\
  "[file normalize "$origin_dir/hdl/common/main/defLEDModule.vhd"]"\
- "[file normalize "$origin_dir/hdl/common/main/defMznInterface.vhd"]"\
+ "[file normalize "$origin_dir/hdl/common/bct_bus_bridge/defMznInterface.vhd"]"\
  "[file normalize "$origin_dir/hdl/common/sitcp/defSiTCP_XG.vhd"]"\
  "[file normalize "$origin_dir/hdl/utility/mikumari/defCDD.vhd"]"\
  "[file normalize "$origin_dir/constrs/pins.xdc"]"\
@@ -537,7 +537,7 @@ set files [list \
  [file normalize "${origin_dir}/hdl/strtdc/lrtdc-impl/strLrTdc.vhd"] \
  [file normalize "${origin_dir}/hdl/toplevel.vhd"] \
  [file normalize "${origin_dir}/hdl/common/main/defLEDModule.vhd"] \
- [file normalize "${origin_dir}/hdl/common/main/defMznInterface.vhd"] \
+ [file normalize "${origin_dir}/hdl/common/bct_bus_bridge/defMznInterface.vhd"] \
  [file normalize "${origin_dir}/hdl/common/sitcp/defSiTCP_XG.vhd"] \
  [file normalize "${origin_dir}/hdl/utility/mikumari/defCDD.vhd"] \
 ]
@@ -1332,7 +1332,7 @@ set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
 set_property -name "library" -value "mylib" -objects $file_obj
 
-set file "$origin_dir/hdl/common/main/defMznInterface.vhd"
+set file "$origin_dir/hdl/common/bct_bus_bridge/defMznInterface.vhd"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
